b8669a19b8f11549c81a8c4a42b0caa547b30bc2
Dylan <dylflanagan@gmail.com>
2023-01-24 12:05:09 -0800

    Color selectors in header imported from coolors

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,17 @@
 import React from 'react';
-
+import { NavLink } from 'react-router-dom';
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <header>
+      <NavLink to="/rgb/24/58/55" className="Rich Black">
+        Rich Black
+      </NavLink>
+      <NavLink to="/rgb/67/37/52" className="Dark Purple">
+        Dark Purple
+      </NavLink>
+      <NavLink to="/rgb/196/73/0" className="Mahogany">
+        Mahogany
+      </NavLink>
+    </header>
+  );
 }
