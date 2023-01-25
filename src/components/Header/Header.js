5906e09e2ade9bc3264d30634905e6d9d6fd7aa4
Dylan <dylflanagan@gmail.com>
2023-01-25 08:49:41 -0800

    Header reflects RGB values of color components

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import './Header.css';
+
 export default function Header() {
   return (
     <header>
-      <NavLink to="/rgb/24/58/55" className="Rich Black">
+      <NavLink to="/rgb/24/58/55" className="rich-black">
         Rich Black
       </NavLink>
-      <NavLink to="/rgb/67/37/52" className="Dark Purple">
+      <NavLink to="/rgb/67/37/52" className="dark-purple">
         Dark Purple
       </NavLink>
-      <NavLink to="/rgb/196/73/0" className="Mahogany">
+      <NavLink to="/rgb/196/73/0" className="mahogany">
         Mahogany
       </NavLink>
     </header>
--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -3,7 +3,7 @@ import './NotFound.css';
 
 export default function NotFound() {
   return (
-    <div>
+    <div className="not-found">
       <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/1be42ca8-42e4-4251-a165-fa748e6ed49a/deotf77-2d1394a5-956a-44ff-b3f0-fa4490b0f65f.png/v1/fill/w_370,h_370,strp/james_sad_face_vector_by_thomastrainfan2006_deotf77-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MzcwIiwicGF0aCI6IlwvZlwvMWJlNDJjYTgtNDJlNC00MjUxLWExNjUtZmE3NDhlNmVkNDlhXC9kZW90Zjc3LTJkMTM5NGE1LTk1NmEtNDRmZi1iM2YwLWZhNDQ5MGIwZjY1Zi5wbmciLCJ3aWR0aCI6Ijw9MzcwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.6QB_k0duVZ92iZrqSF3WRrZj_1K4k4zE9k14LbWM0jY" />
       <h1>Looks like we got off at the wrong stop... </h1>
     </div>
